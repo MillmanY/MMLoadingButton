4149af3772a6ffdf1e216f4fd799bfc7af79ad73
Millman YANG <millmanyang@gmail.com>
2016-10-10 21:25:34 +0800

    Fix UIViewController  Extension error 1.0.4

--- MMLoadingButton/Classes/UIViewControllerExtension.swift
+++ MMLoadingButton/Classes/UIViewControllerExtension.swift
@@ -5,27 +5,9 @@
 //  Created by MILLMAN on 2016/9/5.
 //
 
+
+
 import UIKit
-fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l < r
-  case (nil, _?):
-    return true
-  default:
-    return false
-  }
-}
-
-fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l > r
-  default:
-    return rhs < lhs
-  }
-}
-
 extension UIViewController{
     
     fileprivate static func findBestViewController(_ vc:UIViewController) -> UIViewController! {
@@ -44,7 +26,7 @@ extension UIViewController{
             }
         }
             
-        else if(vc.isKind(of: UINavigationController.classForCoder())){
+        else if(vc.isKind(of:UINavigationController.classForCoder())){
             let svc = vc as! UINavigationController
             if(svc.viewControllers.count > 0){
                 return UIViewController.findBestViewController(svc.topViewController!)
@@ -54,12 +36,11 @@ extension UIViewController{
             }
         }
             
-        else if(vc.isKind(of: UITabBarController.classForCoder())){
-            let svc = vc as! UITabBarController
-            if(svc.viewControllers?.count > 0){
+        else if(vc.isKind(of:UITabBarController.classForCoder())){
+            if let svc = vc as? UITabBarController,let v = svc.viewControllers , v.count > 0{
                 return UIViewController.findBestViewController(svc.selectedViewController!)
-            }
-            else{
+                
+            } else{
                 return vc
             }
         }
@@ -69,10 +50,8 @@ extension UIViewController{
         }
     }
     
-    
     static func currentViewController() -> UIViewController {
         let vc:UIViewController! = UIApplication.shared.keyWindow?.rootViewController
-        
         return UIViewController.findBestViewController(vc)
     }
 }
